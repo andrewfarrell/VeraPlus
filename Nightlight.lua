1876bc7419682f1965db928e2b4e205f5a69986a
AndrewFarrell <andrew.farrell@anz.com>
2019-09-14 15:36:05 +1000

    This is a really small commit

--- Nightlight.lua
+++ Nightlight.lua
@@ -1,8 +1,9 @@
-local BrightnessThreshold = 12
+local BrightnessThreshold = 200
 local LightSensor = 46
 local FrontDoorLight = 11
 local HallwayLightKitchen = 89
 local Delay = 120
+local LightAlreadyOn = 0
 
 function offlight()
     luup.call_action("urn:upnp-org:serviceId:Dimming1", "SetLoadLevelTarget", {newLoadlevelTarget = "0"}, FrontDoorLight)
@@ -10,7 +11,12 @@ function offlight()
 end
 
 CurrentBrightness = tonumber((luup.variable_get("urn:micasaverde-com:serviceId:LightSensor1","CurrentLevel", LightSensor)))
-if (CurrentBrightness < BrightnessThreshold) then   
+LightAlreadyOn =    tonumber((luup.variable_get("urn:upnp-org:serviceId:SwitchPower1","Status", HallwayLightKitchen)))
+-- print("ready")
+-- print(CurrentBrightness)
+-- print(LightAlreadyOn)
+
+if ((CurrentBrightness < BrightnessThreshold) and (LightAlreadyOn < 1)) then   
     luup.call_action("urn:upnp-org:serviceId:Dimming1", "SetLoadLevelTarget", {newLoadlevelTarget = "10"}, FrontDoorLight)
     luup.call_action("urn:upnp-org:serviceId:Dimming1", "SetLoadLevelTarget", {newLoadlevelTarget = "10"}, HallwayLightKitchen)
     luup.call_delay("offlight", Delay) 
