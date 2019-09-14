9065e90a3584fe47f6b1f92d6147b5226f5e4b87
Andrew Farrell <andrew.farrell@anz.com>
2019-09-14 15:17:59 +1000

    First Change

--- Nightlight.lua
+++ Nightlight.lua
@@ -1,1 +1,20 @@
--- test
+local BrightnessThreshold = 12
+local LightSensor = 46
+local FrontDoorLight = 11
+local HallwayLightKitchen = 89
+local Delay = 120
+
+function offlight()
+    luup.call_action("urn:upnp-org:serviceId:Dimming1", "SetLoadLevelTarget", {newLoadlevelTarget = "0"}, FrontDoorLight)
+    luup.call_action("urn:upnp-org:serviceId:Dimming1", "SetLoadLevelTarget", {newLoadlevelTarget = "0"}, HallwayLightKitchen)
+end
+
+CurrentBrightness = tonumber((luup.variable_get("urn:micasaverde-com:serviceId:LightSensor1","CurrentLevel", LightSensor)))
+if (CurrentBrightness < BrightnessThreshold) then   
+    luup.call_action("urn:upnp-org:serviceId:Dimming1", "SetLoadLevelTarget", {newLoadlevelTarget = "10"}, FrontDoorLight)
+    luup.call_action("urn:upnp-org:serviceId:Dimming1", "SetLoadLevelTarget", {newLoadlevelTarget = "10"}, HallwayLightKitchen)
+    luup.call_delay("offlight", Delay) 
+    return (true)
+else
+    return (false)
+end
