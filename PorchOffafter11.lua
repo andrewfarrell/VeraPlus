e0f56562f1ec31052214103dd563989529627f20
AndrewFarrell <andrew.farrell@anz.com>
2019-09-16 21:36:55 +1000

    First Version

---
+++ PorchOffafter11.lua
@@ -0,0 +1,20 @@
+local Delay = 300 -- 5 minutes
+local nightTime = 21 -- Time after which do not turn light back on
+local cTime=tonumber(os.date("%H"))
+-- print(cTime)
+
+function resetlight()
+    cTime=tonumber(os.date("%H"))
+    if ((cTime > nightTime) or (cTime < 6)) then
+        print("Off")
+        -- Turn lights to off
+        luup.call_action("urn:micasaverde-com:serviceId:HomeAutomationGateway1","RunScene",{ SceneNum="40" }, 0)
+    else
+        -- Turn Light to low
+        print("Low")
+        luup.call_action("urn:micasaverde-com:serviceId:HomeAutomationGateway1","RunScene",{ SceneNum="39" }, 0)
+    end 
+end
+
+luup.call_delay("resetlight", Delay) 
+return (true)
