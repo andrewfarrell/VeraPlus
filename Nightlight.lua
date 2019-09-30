5bf37bd6e4cd5ec231b9e0cf5cd51b368a7c5ef4
AndrewFarrell <andrew.farrell@anz.com>
2019-09-30 20:45:08 +1000

    fixed and to an or

--- Nightlight.lua
+++ Nightlight.lua
@@ -1,6 +1,6 @@
+-- Turn night Light on for Delay time in seconds - Triggered on Kitchen Light Sensor (46)
 -- This is a new version with check for light already on
--- Twig
-local BrightnessThreshold = 12
+local BrightnessThreshold = 11
 local LightSensor = 46
 local FrontDoorLight = 11
 local HallwayLightKitchen = 89
@@ -14,11 +14,12 @@ end
 
 CurrentBrightness = tonumber((luup.variable_get("urn:micasaverde-com:serviceId:LightSensor1","CurrentLevel", LightSensor)))
 LightAlreadyOn =    tonumber((luup.variable_get("urn:upnp-org:serviceId:SwitchPower1","Status", HallwayLightKitchen)))
--- print("ready")
--- print(CurrentBrightness)
--- print(LightAlreadyOn)
+print("ready")
+print(CurrentBrightness)
+print(LightAlreadyOn)
 
-if ((CurrentBrightness < BrightnessThreshold) and (LightAlreadyOn < 1)) then   
+
+if ((CurrentBrightness < BrightnessThreshold) or (LightAlreadyOn < 1)) then   
     luup.call_action("urn:upnp-org:serviceId:Dimming1", "SetLoadLevelTarget", {newLoadlevelTarget = "10"}, FrontDoorLight)
     luup.call_action("urn:upnp-org:serviceId:Dimming1", "SetLoadLevelTarget", {newLoadlevelTarget = "10"}, HallwayLightKitchen)
     luup.call_delay("offlight", Delay) 
